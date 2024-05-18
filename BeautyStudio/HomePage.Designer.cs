3c3e10786b3c2651741868d77d787d878ecdc352
RositsaSlavova <slavova02@gmail.com>
2024-05-18 13:57:44 +0300

    added new buttons to HomePage

---
+++ BeautyStudio/HomePage.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace BeautyStudio
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            label1 = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            button1 = new Button();
+            panel5 = new Panel();
+            button2 = new Button();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            button3 = new Button();
+            flowLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            panel7.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.FromArgb(183, 130, 187);
+            flowLayoutPanel1.Controls.Add(panel1);
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Controls.Add(panel3);
+            flowLayoutPanel1.Controls.Add(panel4);
+            flowLayoutPanel1.Controls.Add(panel5);
+            flowLayoutPanel1.Controls.Add(panel6);
+            flowLayoutPanel1.Controls.Add(panel7);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(250, 603);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(247, 125);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(247, 125);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(3, 134);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(247, 50);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Impact", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(247, 50);
+            label1.TabIndex = 0;
+            label1.Text = "Hi, Username";
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(3, 190);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(247, 22);
+            panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(button1);
+            panel4.Location = new Point(3, 218);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(247, 50);
+            panel4.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(247, 50);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(button2);
+            panel5.Location = new Point(3, 274);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(247, 50);
+            panel5.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(0, 0);
+            button2.Name = "button2";
+            button2.Size = new Size(247, 50);
+            button2.TabIndex = 0;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel6
+            // 
+            panel6.Location = new Point(3, 330);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(247, 178);
+            panel6.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            panel7.Controls.Add(button3);
+            panel7.Location = new Point(3, 514);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(247, 50);
+            panel7.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(0, 0);
+            button3.Name = "button3";
+            button3.Size = new Size(247, 50);
+            button3.TabIndex = 0;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // HomePage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1010, 603);
+            Controls.Add(flowLayoutPanel1);
+            Name = "HomePage";
+            Text = "HomePage";
+            flowLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel7.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private Label label1;
+        private Panel panel3;
+        private Panel panel4;
+        private Button button1;
+        private Panel panel5;
+        private Button button2;
+        private Panel panel6;
+        private Panel panel7;
+        private Button button3;
+    }
+}
